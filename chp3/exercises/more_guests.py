a2a350246d82876bda1b6719c7e4aeb91c5ed07b
Paulo Couto <biofobico@protonmail.com>
2021-04-04 23:49:16 +0100

    primeiro commito do vscode

--- chp3/exercises/more_guests.py
+++ chp3/exercises/more_guests.py
@@ -1,14 +1,49 @@
-guest_list = ['beethoven', 'david bowie', 'freddy mercury', 'mozart']
-print("I've found a bigger table so I'll invite more people.")
+guests = ['bob marley', 'david bowie', 'freddy mercury']
 
-guest_list.insert(0, 'chopin')
-guest_list.insert(3, 'vivaldi')
-guest_list.append('bach')
+name = guests[0].title()
+print(f"{name} please come dinner.")
 
-print(f"\nHello {guest_list[0].title()}, would you like to have a dinner with us?")
-print(f"Good morning {guest_list[1].title()}, would you like to have a dinner with us?")
-print(f"Hi {guest_list[2].title()}, would you like to have a dinner with us?")
-print(f"Howdy {guest_list[3].title()}, would you like to have a dinner with us?")
-print(f"Hello {guest_list[4].title()}, would you like to have a dinner with us?")
-print(f"Good afternoon {guest_list[5].title()}, would you like to have a dinner with us?")
-print(f"Hi there {guest_list[-1].title()}, would you like to have a dinner with us?")
+name = guests[1].title()
+print(f"{name} please come dinner.")
+
+name = guests[2].title()
+print(f"{name} please come dinner.")
+
+print(f"\n{guests[2].title()} can't come to dinner.")
+
+del guests[-1]
+guests.append('bach')
+
+name = guests[0].title()
+print(f"\n{name} please come dinner.")
+
+name = guests[1].title()
+print(f"{name} please come dinner.")
+
+name = guests[2].title()
+print(f"{name} please come dinner.")
+
+print("\nHey I've found a bigger table!")
+
+guests.insert(0, 'chopin')
+guests.insert(2, 'vivaldi')
+guests.append('strauss')
+
+msg = guests[0].title()
+print(f"\n{msg} please come dinner.")
+
+msg = guests[1].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[2].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[3].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[4].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[5].title()
+print(f"{msg} please come dinner.")
+
