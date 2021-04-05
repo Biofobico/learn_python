efa7a414897a225d9471c0c93b8e22092a17b15d
Paulo Couto <biofobico@protonmail.com>
2021-04-05 09:31:19 +0100

    fim do exercicio Shrinking guest list

--- chp3/exercises/shrinking_guest_list.py
+++ chp3/exercises/shrinking_guest_list.py
@@ -1,14 +1,72 @@
-guest_list = ['beethoven', 'david bowie', 'freddy mercury', 'mozart']
-print("I'm sorry but I can only invite two people for dinner")
+guests = ['bob marley', 'david bowie', 'freddy mercury']
 
-mozart_poped = guest_list.pop()
-print(f"\nI'm sorry {mozart_poped.title()} but you are uninvited for dinner.")
+name = guests[0].title()
+print(f"{name} please come dinner.")
 
-freddy_poped = guest_list.pop()
-print(f"Sorry {freddy_poped.title()}, you can't have dinner with us.")
+name = guests[1].title()
+print(f"{name} please come dinner.")
 
-print(f"\n Hello {guest_list[0].title()} and {guest_list[1].title()}. You're sill invited for dinner.")
+name = guests[2].title()
+print(f"{name} please come dinner.")
 
-del guest_list[0]
-del guest_list[0]
-print(f"\n{guest_list}")
+print(f"\n{guests[2].title()} can't come to dinner.")
+
+del guests[-1]
+guests.append('bach')
+
+name = guests[0].title()
+print(f"\n{name} please come dinner.")
+
+name = guests[1].title()
+print(f"{name} please come dinner.")
+
+name = guests[2].title()
+print(f"{name} please come dinner.")
+
+print("\nHey I've found a bigger table!")
+
+guests.insert(0, 'chopin')
+guests.insert(2, 'vivaldi')
+guests.append('strauss')
+
+msg = guests[0].title()
+print(f"\n{msg} please come dinner.")
+
+msg = guests[1].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[2].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[3].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[4].title()
+print(f"{msg} please come dinner.")
+
+msg = guests[5].title()
+print(f"{msg} please come dinner.")
+
+print("\nI can only invite two people")
+
+removed = guests.pop()
+print(f"\nSorry {removed.title()} you won't dinner.")
+
+removed = guests.pop()
+print(f"\nSorry {removed.title()} you won't dinner with us.")
+
+removed = guests.pop()
+print(f"\nSorry {removed.title()} you won't dinner.")
+
+removed = guests.pop()
+print(f"\nSorry {removed.title()} you won't dinner.")
+
+invited = guests[0].title()
+print(f"\n{invited} you are still invited.")
+
+invited = guests[1].title()
+print(f"\n{invited} you are still invited.")
+
+del(guests[0])
+del(guests[0])
+print(guests)
