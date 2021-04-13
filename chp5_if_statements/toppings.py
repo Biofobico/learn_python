e2ba65f88123922110c77efbaf394f48f85a1ddd
Biofobico <biofobico@protonmail.com>
2021-04-13 22:08:06 +0100

    added more code to toppings.py

--- chp5_if_statements/toppings.py
+++ chp5_if_statements/toppings.py
@@ -13,3 +13,15 @@ if 'extra cheese' in toppings:
     print("Adding extra cheese.")
 
 print("\nFinished making your pizza!")
+
+print("\n")
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are out of green peppers right now.")
+    else:
+        print(f"Adding {requested_toppings}.")
+
+print("\nFinished making your pizza.")
