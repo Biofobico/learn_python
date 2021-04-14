086d5075e97a46b64fbe6df41d49acc0ad261e3d
Biofobico <biofobico@protonmail.com>
2021-04-14 11:17:45 +0100

    add more to toppings.py

--- chp5_if_statements/toppings.py
+++ chp5_if_statements/toppings.py
@@ -25,3 +25,27 @@ for requested_topping in requested_toppings:
         print(f"Adding {requested_toppings}.")
 
 print("\nFinished making your pizza.")
+
+print("\n")
+
+requested_toppings = []
+
+if requested_toppings:
+    for resquested_topping in requested_toppings:
+        print(f"Adding {requested_topping}.")
+    print("\nFinished making your pizza.")
+else:
+    print("Are you sure you want a plain pizza?")
+
+# Multiple lists
+available_toppings = ['mushrooms', "olives", 'green peppers',
+            'pepperoni', 'pineapple', 'extra cheese']
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
+
+for requested_topping in requested_toppings:
+    if requested_topping in available_toppings:
+        print(f"Adding {requested_topping}.")
+    else:
+        print(f"Sorry we don't have {requested_topping}")
+
+print("\nFinished making your pizza.")
