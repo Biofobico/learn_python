fd802d72b7d290f891c579970b64f7e255dcf53c
Biofobico <biofobico@protonmail.com>
2021-04-16 08:35:05 +0100

    novos exercicios chapter 5

---
+++ chp5_if_statements/exercises/hello_admin.py
@@ -0,0 +1,10 @@
+usernames = [
+    'shaman', 'choppa', 'admin', 'black orc', 'chosen', 'zealot', 'magus',
+    'marauder'
+]
+
+for username in usernames:
+    if username == 'admin':
+        print(f"Hello {username}, would you like to see a status report?")
+    else:
+        print(f"Hello {username.title()}, thank you for logging in again.")
